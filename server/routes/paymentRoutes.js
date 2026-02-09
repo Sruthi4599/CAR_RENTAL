df67946409d3b81a7fcdf3fc8fa2df80000a5b20
Sruthi4599 <kovvurisaisruthi@gmail.com>
2026-02-09 21:55:30 +0530

    cors

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -3,6 +3,12 @@ import { fakePayment } from "../controller/paymentController.js";
 
 const router = express.Router();
 
+// ✅ allow preflight
+router.options("/fake-payment", (req, res) => {
+  res.sendStatus(200);
+});
+
+// ✅ actual payment
 router.post("/fake-payment", fakePayment);
 
 export default router;
