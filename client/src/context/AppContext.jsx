570792150713bab8032ec47ef10d8ad1995d6ff4
Sruthi4599 <kovvurisaisruthi@gmail.com>
2026-02-18 22:32:00 +0530

    add car

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -24,7 +24,7 @@ export const AppProvider = ({ children }) => {
       const { data } = await axios.get("/api/users/data");
       if (data.success) {
         setUser(data.user);
-        setIsOwner(data.user.roles?.includes("owner"));
+        setIsOwner(true);
       } else {
         setUser(null);
         setIsOwner(false);
